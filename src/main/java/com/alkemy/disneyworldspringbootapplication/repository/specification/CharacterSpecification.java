5c1f8e26bb291f0e8fbc5b2e2d1dd4c2f4df1445
ezevillafae <eze.villafae07@gmail.com>
2022-07-10 19:33:05 -0300

    added character filter

--- src/main/java/com/alkemy/disneyworldspringbootapplication/repository/specification/CharacterSpecification.java
+++ src/main/java/com/alkemy/disneyworldspringbootapplication/repository/specification/CharacterSpecification.java
@@ -2,12 +2,18 @@ package com.alkemy.disneyworldspringbootapplication.repository.specification;
 
 import com.alkemy.disneyworldspringbootapplication.dto.CharacterFilterDto;
 import com.alkemy.disneyworldspringbootapplication.entity.CharacterEntity;
+import com.alkemy.disneyworldspringbootapplication.entity.FilmEntity;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Predicate;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Component
@@ -24,6 +30,33 @@ public class CharacterSpecification {
                             "%" + filterDto.getName().toLowerCase() + "%"
                 ));
             }
+
+            if(StringUtils.hasLength(filterDto.getId())){
+                predicateList.add(
+                        criteriaBuilder.equal(
+                                root.get("id"),
+                                filterDto.getId()
+                        )
+                );
+            }
+
+            if(StringUtils.hasLength(filterDto.getAge())){
+                predicateList.add(
+                        criteriaBuilder.equal(
+                                root.get("age"),
+                                filterDto.getAge()
+                        )
+                );
+            }
+
+            if(!CollectionUtils.isEmpty(filterDto.getMovies())){
+                Join<FilmEntity, CharacterEntity> join = root.join("films", JoinType.INNER);
+                Expression<String> filmsId = join.get("id");
+                predicateList.add(filmsId.in(filterDto.getMovies()));
+            }
+
+            query.distinct(true);
+
 
             return criteriaBuilder.and(predicateList.toArray(new Predicate[0]));
         };
